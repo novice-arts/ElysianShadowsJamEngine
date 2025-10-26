4f007427e0a6f4e0d17244f1a30c4a6556a35bd9
Novice Arts LLC <noviceartsstudios@gmail.com>
2025-10-26 16:29:57 -0400

    updating app structure in main entry point to be more akin to callback structure
    Signed-off-by: Novice Arts LLC <noviceartsstudios@gmail.com>

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
-#ifdef __EMSCRIPTEN__
-#include <emscripten.h>
-#endif
+
 
 #include <SDL3/SDL.h>
 #include <stdio.h>
 
 #include "src/engine.h"
+#include "src/app.h"
 
 // Main loop flag
 int quit = 0;
@@ -14,87 +13,19 @@ SDL_Event e;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
 
-#ifdef __EMSCRIPTEN__
-#define mainloop() emscripten_set_main_loop(mainloop, 0, 1);
-static void (mainloop)(void){
-	if(!quit){
-		// Handle events on queue
-        while (SDL_PollEvent(&e) != 0) {
-            // User requests quit
-            if (e.type == SDL_EVENT_QUIT) {
-                quit = 1;
-            }
-        }
-
-        // Clear screen
-		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF); // green
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF); // Red
-		//SDL_RenderFillRect(renderer, &(SDL_FRect){0.0f, 0.0f, 200.0f, 200.0f});
-		RECT2D_GET_CLASS(my_rect)->pFnDraw(my_rect, renderer);
-
-        // Update screen
-        SDL_RenderPresent(renderer);
-	} else{
-		emscripten_cancel_main_loop();
-		// Destroy renderer
-		SDL_DestroyRenderer(renderer);
-
-		// Destroy window
-		SDL_DestroyWindow(window);
-
-		// Quit SDL subsystems
-		SDL_Quit();
-	}
-	
-}
-#else 
-static void (mainloop)(void){
-	// Main loop
-    while (!quit) {
-        // Handle events on queue
-        while (SDL_PollEvent(&e) != 0) {
-            // User requests quit
-            if (e.type == SDL_EVENT_QUIT) {
-                quit = 1;
-            }
-        }
-
-        // Clear screen
-		SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF); // green
-        SDL_RenderClear(renderer);
-        SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF); // Red
-		//SDL_RenderFillRect(renderer, &(SDL_FRect){0.0f, 0.0f, 200.0f, 200.0f});
-		RECT2D_GET_CLASS(my_rect)->pFnDraw(my_rect, renderer);
-
-        // Update screen
-        SDL_RenderPresent(renderer);
-    }
-	// Destroy renderer
-    SDL_DestroyRenderer(renderer);
-
-    // Destroy window
-    SDL_DestroyWindow(window);
-
-    // Quit SDL subsystems
-    SDL_Quit();
-}
-#endif
-
-int main(int argc, char* argv[]){
-
-    // Initialize SDL
+static inline bool (app_setup)(void){
+	// Initialize SDL
     if (!SDL_Init(SDL_INIT_VIDEO)) {
         fprintf(stderr, "SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-        return 1;
+        return false;
     }
 
     // Create window
-    window = SDL_CreateWindow("SDL3 Window Example", 800, 600, SDL_WINDOW_RESIZABLE);
+    window = SDL_CreateWindow("SDL3 Window Example", 640, 480, SDL_WINDOW_RESIZABLE);
     if (window == NULL) {
         fprintf(stderr, "Window could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_Quit();
-        return 1;
+        return false;
     }
 
     // Create renderer
@@ -103,13 +34,62 @@ int main(int argc, char* argv[]){
         fprintf(stderr, "Renderer could not be created! SDL_Error: %s\n", SDL_GetError());
         SDL_DestroyWindow(window);
         SDL_Quit();
-        return 1;
+        return false;
+    }
+	my_rect = Rect2D_create("x", 50.0f, "y", 50.0f, "w", 150.0f, "h", 150.0f);
+	return true;
+}
+
+static inline void (app_update)(void){
+	// Handle events on queue
+    while (SDL_PollEvent(&e) != 0) {
+        // User requests quit
+        if (e.type == SDL_EVENT_QUIT) {
+            quit = 1;
+        }
     }
 
-    
-	my_rect = Rect2D_create("x", 50.0f, "y", 50.0f, "w", 150.0f, "h", 150.0f);
-    
-	mainloop();
+    // Clear screen
+	SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0xFF, 0xFF); // green
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0x00, 0xFF); // Red
+	//SDL_RenderFillRect(renderer, &(SDL_FRect){0.0f, 0.0f, 200.0f, 200.0f});
+	RECT2D_GET_CLASS(my_rect)->pFnDraw(my_rect, renderer);
+
+    // Update screen
+    SDL_RenderPresent(renderer);
+}
+
+static inline void (app_teardown)(void){
+	// Destroy renderer
+	SDL_DestroyRenderer(renderer);
+
+	// Destroy window
+	SDL_DestroyWindow(window);
+
+	// Quit SDL subsystems
+	SDL_Quit();
+}
+
+#ifdef __EMSCRIPTEN__
+	#include <emscripten.h>
+	#define app_loop() emscripten_set_main_loop(app_loop, 0, 1);
+	#define app_teardown() emscripten_cancel_main_loop(); (app_teardown)();
+#else
+	#define app_loop() do { (app_loop)(); }while(!quit)
+#endif
+
+static inline void (app_loop)(void){
+	app_update();	 
+	if(quit){
+		app_teardown();
+	}
+}
+
+int main(int argc, char* argv[]){    
+	if(!app_setup())
+		return 1;
+	app_loop();
 
 	return 0;
 }
---
+++ src/app.h
@@ -0,0 +1,10 @@
+#ifndef APP_H
+#define APP_H
+
+static inline bool (app_setup)(void);
+static void (app_loop)(void);
+static inline void (app_teardown)(void);
+
+static inline void (app_update)(void);
+
+#endif //APP_H
